774cf8f3c1d089f89757877f5722931ce7fb2827
zrufael <barokzrufael@gmail.com>
2023-03-26 14:39:26 +0300

    python code

---
+++ gtst.py
@@ -0,0 +1,14 @@
+gtst = "Day7"
+
+print("Hello today is our " + gtst + " course")
+
+y = [0,2,4]
+print("the 1st even number is ",y[0])
+print("the 2nd even number is ",y[1])
+print("the 3rd even number is ",y[2])
+
+Fruit = {'Apple':10,'Banana':15,'Pineapple':20}
+#Beware while inputing since Python is case-sensetive !!!  
+Choice = input('Choose the fruit that you want to know the value ? \n')
+print('\n The value of ' + Choice + ' is :', (Fruit[Choice]) )
+ 
